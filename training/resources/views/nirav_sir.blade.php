902d4e1cb37564d894db2e9efbd415745773f49c
darshan <darshan.patel.tops@gmail.com>
2023-10-30 15:31:08 +0530

    Update

---
+++ training/resources/views/nirav_sir.blade.php
@@ -0,0 +1,38 @@
+<h1>Welcome nirav sir{{ dd(100+320) }}</h1>
+
+{{-- {{ dd("gello") }} --}}
+
+
+{{-- @foreach ($collection as $item)
+
+@endforeach
+
+@if ()
+
+@else
+
+@endif
+
+@forelse ($collection as $item)
+
+@empty
+
+@endforelse
+
+
+@switch($type)
+    @case(1)
+
+        @break
+    @case(2)
+
+        @break
+    @default
+
+@endswitch --}}
+
+<script>
+
+console.log("hello");
+
+</script>
